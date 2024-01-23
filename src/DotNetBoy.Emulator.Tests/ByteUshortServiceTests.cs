75ff4bee8d0f3abbd0668a13929b229c467ab5ee
viccie211 <victorremmerswaal@hotmail.com>
2024-01-23 09:26:58 +0100

    Completed coverage of ByteUshortService

--- src/DotNetBoy.Emulator.Tests/ByteUshortServiceTests.cs
+++ src/DotNetBoy.Emulator.Tests/ByteUshortServiceTests.cs
@@ -41,4 +41,65 @@ public class ByteUshortServiceTests
         var result = service.CombineBytes(upper, lower);
         Assert.That(result, Is.EqualTo(expected));
     }
+
+    [Test]
+    public void LowerByteOfSixteenBits0()
+    {
+        var service = new ByteUshortService();
+        ushort source = 0;
+        var expected = 0;
+        var result = service.LowerByteOfSixteenBits(source);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void LowerByteOfSixteenBitsFF()
+    {
+        var service = new ByteUshortService();
+        ushort source = 0x00FF;
+        var expected = 0xFF;
+        var result = service.LowerByteOfSixteenBits(source);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void LowerByteOfSixteenBitsOnlyLowerByte()
+    {
+        var service = new ByteUshortService();
+        ushort source = 0xFF01;
+        var expected = 1;
+        var result = service.LowerByteOfSixteenBits(source);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void UpperByteOfSixteenBits0()
+    {
+        var service = new ByteUshortService();
+        ushort source = 0;
+        var expected = 0;
+        var result = service.UpperByteOfSixteenBits(source);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void UpperByteOfSixteenBitsFF()
+    {
+        var service = new ByteUshortService();
+        ushort source = 0xFF00;
+        var expected = 0xFF;
+        var result = service.UpperByteOfSixteenBits(source);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void UpperByteOfSixteenBitsOnlyUpperByte()
+    {
+        var service = new ByteUshortService();
+        ushort source = 0x01FF;
+        var expected = 0x01;
+        var result = service.UpperByteOfSixteenBits(source);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
 }
