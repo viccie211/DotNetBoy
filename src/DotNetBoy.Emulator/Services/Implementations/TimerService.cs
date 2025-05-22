a7dcaae2e7e08138cb346c97998029748b0bb225
viccie211 <victorremmerswaal@hotmail.com>
2025-05-22 19:35:29 +0200

    Sorted out licences
    Properly included the license of SpecBoy in the piece of code that I actually borrowed.

--- src/DotNetBoy.Emulator/Services/Implementations/TimerService.cs
+++ src/DotNetBoy.Emulator/Services/Implementations/TimerService.cs
@@ -2,9 +2,34 @@
 
 namespace DotNetBoy.Emulator.Services.Implementations;
 
+/*
+ * This class currently makes heavy use of code written by spec-chum in their SpecBoy project.
+ * I'm due to refactor this back to my own implementation but for now it is what it is.
+ * For fullness this is the LICENSE found in SpecBoy's Repo accessed on 2025-05-22
+MIT License
+
+Copyright (c) 2020 spec-chum
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 public class TimerService : ITimerService
 {
-    // TimerService implementation is mostly based on Timers.cs from SpecBoy. I couldn't get my own implementation to work properly, so I stole it :/.
     private static readonly int[] triggerBits = [9, 3, 5, 7];
 
     private int divBit;
